f468535de2bfb6b9b7f6536b43498e1b5c1844e0
jamese21 <jamese@student.cis.edu.hk>
2020-04-05 17:05:11 +0800

    In progress creating the layouts, doing colours.

--- app/src/main/java/com/example/myapplication/ItemActivity.java
+++ app/src/main/java/com/example/myapplication/ItemActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AdminActivity extends AppCompatActivity {
+public class ItemActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin);
+        setContentView(R.layout.activity_item);
     }
 }
